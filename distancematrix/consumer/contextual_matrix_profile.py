07e8bca27f32b4811729477a7475a6d4e95252e1
Dieter De Paepe <dieter.depaepe@gmail.com>
2018-07-09 10:44:58 +0200

    Implement contextual matrix profile.

---
+++ distancematrix/consumer/contextual_matrix_profile.py
@@ -0,0 +1,138 @@
+import collections
+import numpy as np
+from .abstract_consumer import AbstractConsumer
+
+
+class ContextualMatrixProfile(AbstractConsumer):
+    """
+    A consumer that constructs the conceptual matrix profile. The conceptual matrix profile is formed by
+    taking the minimum of rectangles across the full distance matrix (where the matrix profile takes the
+    minimum across columns).
+    """
+
+    def __init__(self, series_contexts, query_contexts=None):
+        """
+        Creates a new consumer that calculates a contextual matrix profile, along with corresponding indices.
+
+        :param series_contexts: an iterable of ranges, each range defines one context. You can also
+          use lists of ranges, to specify non-consecutive contexts.
+        :param query_contexts: iterable of ranges, defaults to None, meaning to use the same contexts as the series
+        """
+        self._num_subseq = None
+        self._range = None
+
+        self._verify_ranges([r for i, r in _enumerate_flattened(series_contexts)])
+
+        if query_contexts is None:
+            query_contexts = series_contexts
+        else:
+            self._verify_ranges([r for i, r in _enumerate_flattened(query_contexts)])
+
+        self._series_contexts = np.array(
+            [(r.start, r.stop, i) for i, r in _enumerate_flattened(series_contexts)], dtype=np.int)
+        self._query_contexts = np.array(
+            [(r.start, r.stop, i) for i, r in _enumerate_flattened(query_contexts)], dtype=np.int)
+
+        self._qc_sorted_start = self._query_contexts[np.argsort(self._query_contexts[:, 0])]
+        self._qc_sorted_stop = self._query_contexts[np.argsort(self._query_contexts[:, 1])]
+
+        self.distance_matrix = None
+        self.match_index_series = None
+        self.match_index_query = None
+
+    def initialise(self, series, query, m):
+        n = len(series)
+        q = len(query)
+
+        self._num_subseq = n - m + 1
+        self._range = np.arange(0, max(n - m + 1, q - m + 1), dtype=np.int)
+
+        num_series_contexts = np.max(self._series_contexts[:, 2]) + 1
+        num_query_contexts = np.max(self._query_contexts[:, 2]) + 1
+
+        self.distance_matrix = np.full((num_query_contexts, num_series_contexts), np.Inf, dtype=np.float)
+        self.match_index_series = np.full((num_query_contexts, num_series_contexts), -1, dtype=np.int)
+        self.match_index_query = np.full((num_query_contexts, num_series_contexts), -1, dtype=np.int)
+
+    def _verify_ranges(self, ranges):
+        for r in ranges:
+            if r.step != 1:
+                raise RuntimeError("Only ranges with step 1 supported.")
+            if r.start < 0:
+                raise RuntimeError("Range start should not be negative.")
+
+    def process_diagonal(self, diag, values):
+        num_values = len(values)
+
+        if diag >= 0:
+            values_idx0_start = 0  # Absolute index where values belong in the distance matrix (first index)
+            values_idx1_start = diag  # Absolute index where values belong in the distance matrix (2nd index)
+            # All contexts that start before last value passed
+            context0_idxs = self._qc_sorted_start[0: np.searchsorted(self._qc_sorted_start[:, 0], num_values)]
+        else:
+            values_idx0_start = -diag
+            values_idx1_start = 0
+            # All contexts whose end is on or after the first value passed
+            context0_idxs = self._qc_sorted_stop[
+                            np.searchsorted(self._qc_sorted_stop[:, 1], values_idx0_start, side="right"):]
+
+        for c0_start, c0_end, c0_identifier in context0_idxs:
+            # We now have a sub-sequence (ss) defined by the first context on the query axis
+            # In absolute coordinates, start/end of this subsequence on 2nd axis (series axis)
+            ss1_start = max(0, c0_start + diag)
+            ss1_end = min(self._num_subseq, c0_end + diag)
+
+            context1_idxs = self._series_contexts[np.logical_and(
+                self._series_contexts[:, 0] < ss1_end,  # Start of context is before end of sequence
+                self._series_contexts[:, 1] > ss1_start  # End of context is after start of sequence
+            )]
+
+            for c1_start, c1_end, c1_identifier in context1_idxs:
+                # In absolute coordinates, start/end of the subsequence on 2nd axis defined by both contexts
+                sss1_start = max(ss1_start, c1_start)
+                sss1_end = min(ss1_end, c1_end)
+
+                # Values that belong to both contexts
+                sss_values = values[sss1_start - values_idx1_start: sss1_end - values_idx1_start]
+
+                # Compare if better than current
+                min_sss_value = np.min(sss_values)
+                is_better = min_sss_value < self.distance_matrix[c0_identifier, c1_identifier]
+
+                if is_better:
+                    self.distance_matrix[c0_identifier, c1_identifier] = min_sss_value
+                    rel_indices = np.argmin(sss_values)
+                    sss0_start = sss1_start - diag
+                    self.match_index_query[c0_identifier, c1_identifier] = rel_indices + sss0_start
+                    self.match_index_series[c0_identifier, c1_identifier] = rel_indices + sss1_start
+
+    def process_column(self, column_index, values):
+        context1_idxs = self._series_contexts[np.logical_and(
+            self._series_contexts[:, 0] < column_index + 1,  # Start of context is on or before column
+            self._series_contexts[:, 1] > column_index  # End of context is after column
+        )]
+
+        for _, _, c1_identifier in context1_idxs:
+            for c0_start, c0_end, c0_identifier in self._query_contexts:
+                subseq = values[c0_start: c0_end]
+                best_value = np.min(subseq)
+
+                if best_value < self.distance_matrix[c0_identifier, c1_identifier]:
+                    self.distance_matrix[c0_identifier, c1_identifier] = best_value
+                    self.match_index_query[c0_identifier, c1_identifier] = np.argmin(subseq) + c0_start
+                    self.match_index_series[c0_identifier, c1_identifier] = column_index
+
+
+def _enumerate_flattened(l):
+    """
+    Converts a list of elements and lists into tuples (index, element), so that elements in nested lists
+    have the same index.
+
+    Eg: [1, [2,3], 4] => (0, 1), (1, 2), (1, 3), (2, 4)
+    """
+    for i, el in enumerate(l):
+        if isinstance(el, collections.Iterable) and not isinstance(el, range):
+            for r in el:
+                yield i, r
+        else:
+            yield i, el
---
+++ distancematrix/tests/consumer/test_contextual_matrix_profile.py
@@ -0,0 +1,176 @@
+import numpy as np
+from unittest import TestCase
+import numpy.testing as npt
+
+from distancematrix.util import diag_indices_of
+from distancematrix.consumer.contextual_matrix_profile import ContextualMatrixProfile
+
+
+class TestContextualMatrixProfile(TestCase):
+
+    def setUp(self):
+        self.dist_matrix = np.array([
+            [8.67, 1.10, 1.77, 1.26, 1.91, 4.29, 6.32, 4.24, 4.64, 5.06, 6.41, 4.07, 4.67, 9.32, 5.09],
+            [4.33, 4.99, 0.14, 2.79, 2.10, 6.26, 9.40, 4.14, 5.53, 4.26, 8.21, 5.91, 6.83, 9.26, 6.19],
+            [0.16, 9.05, 1.35, 4.78, 7.01, 4.36, 5.24, 8.81, 7.90, 5.84, 8.90, 7.88, 3.37, 4.70, 6.94],
+            [0.94, 8.70, 3.87, 6.29, 0.32, 1.79, 5.80, 2.61, 1.43, 6.32, 1.62, 0.20, 2.28, 7.11, 2.15],
+            [9.90, 4.51, 2.11, 2.83, 5.52, 8.55, 6.90, 0.24, 1.58, 4.26, 8.75, 3.71, 9.93, 8.33, 0.38],
+            [7.30, 5.84, 9.63, 1.95, 3.76, 3.61, 9.42, 5.56, 5.09, 7.07, 1.90, 4.78, 1.06, 0.69, 3.67],
+            [2.17, 8.37, 3.99, 4.28, 4.37, 2.86, 8.61, 3.39, 8.37, 6.95, 6.57, 1.79, 7.40, 4.41, 7.64],
+            [6.26, 0.29, 6.44, 8.84, 1.24, 2.52, 6.25, 3.07, 5.55, 3.19, 8.16, 5.32, 9.01, 0.39, 9.],
+            [4.67, 8.88, 3.05, 3.06, 2.36, 8.34, 4.91, 5.46, 9.25, 9.78, 0.03, 5.64, 5.10, 3.58, 6.92],
+            [1.01, 0.91, 6.28, 7.79, 0.68, 5.50, 6.72, 5.11, 0.80, 9.30, 9.77, 4.71, 3.26, 7.29, 6.26]])
+
+        self.m = 5
+
+    def mock_initialise(self, cdm):
+        mock_series = np.zeros(self.dist_matrix.shape[1] + self.m - 1)
+        mock_query = np.zeros(self.dist_matrix.shape[0] + self.m - 1)
+        cdm.initialise(mock_series, mock_query, self.m)
+
+    def bruteforce_cdm(self, dist_matrix, query_ranges, series_ranges):
+        """
+        Brute force calculation of contextual distance matrix.
+        :param dist_matrix: 2D matrix
+        :param query_ranges: list of length m containing: ranges or list of ranges
+        :param series_ranges: list of length n containing: ranges or list of ranges
+        :return: 2D matrix (m by n)
+        """
+
+        correct = np.full((len(query_ranges), len(series_ranges)), np.inf, dtype=np.float)
+        correct_qi = np.full((len(query_ranges), len(series_ranges)), -1, dtype=np.int)
+        correct_si = np.full((len(query_ranges), len(series_ranges)), -1, dtype=np.int)
+
+        for i, r0s in enumerate(query_ranges):
+            if not isinstance(r0s, list):
+                r0s = [r0s]
+
+            for r0 in r0s:
+                for j, r1s in enumerate(series_ranges):
+                    if not isinstance(r1s, list):
+                        r1s = [r1s]
+
+                    for r1 in r1s:
+                        view = dist_matrix[r0, :][:, r1]
+                        min_value = np.min(view)
+
+                        if correct[i, j] > min_value:
+                            correct[i, j] = np.min(view)
+                            correct_qi[i, j], correct_si[i, j] = next(zip(*np.where(view == np.min(view))))
+                            correct_qi[i, j] += r0.start
+                            correct_si[i, j] += r1.start
+
+        return correct, correct_qi, correct_si
+
+    def test_process_diagonal(self):
+        query_ranges = [range(0, 4), range(7, 8)]
+        series_ranges = [range(1, 4), range(6, 8)]
+
+        correct, correct_qi, correct_si = self.bruteforce_cdm(self.dist_matrix, query_ranges, series_ranges)
+
+        cdm = ContextualMatrixProfile(series_ranges, query_ranges)
+        self.mock_initialise(cdm)
+
+        for diag in range(-self.dist_matrix.shape[0] + 1, self.dist_matrix.shape[1]):
+            diag_ind = diag_indices_of(self.dist_matrix, diag)
+            cdm.process_diagonal(diag, self.dist_matrix[diag_ind])
+
+        npt.assert_allclose(cdm.distance_matrix, correct)
+        npt.assert_equal(cdm.match_index_query, correct_qi)
+        npt.assert_equal(cdm.match_index_series, correct_si)
+
+    def test_process_diagonal_partial_calculation(self):
+        # Ranges selected so that the diagonals have a mix of the number of numbers that are filled in
+        query_ranges = [range(0, 2), range(2, 5), range(7, 8), range(8, 9), range(9, 10)]
+        series_ranges = [range(0, 2), range(2, 4), range(4, 10), range(13, 14)]
+
+        part_dist_matrix = np.full_like(self.dist_matrix, np.inf)
+
+        cdm = ContextualMatrixProfile(series_ranges, query_ranges)
+        self.mock_initialise(cdm)
+
+        for diag in range(-8, self.dist_matrix.shape[1], 4):
+            diag_ind = diag_indices_of(self.dist_matrix, diag)
+            cdm.process_diagonal(diag, self.dist_matrix[diag_ind])
+            part_dist_matrix[diag_ind] = self.dist_matrix[diag_ind]
+
+        correct, correct_qi, correct_si = self.bruteforce_cdm(part_dist_matrix, query_ranges, series_ranges)
+
+        npt.assert_allclose(cdm.distance_matrix, correct)
+        npt.assert_equal(cdm.match_index_query, correct_qi)
+        npt.assert_equal(cdm.match_index_series, correct_si)
+
+    def test_process_diagonal_complex_ranges(self):
+        # Overlapping ranges and contexts consisting of multiple ranges
+        query_ranges = [range(0, 10), range(1, 5), range(1, 2), range(4, 5),
+                        [range(1, 2), range(3, 4), range(7, 9)]]
+        series_ranges = [range(0, 2), range(1, 3), range(2, 4), range(3, 6), range(4, 8), range(4, 10),
+                         [range(0, 3), range(3, 5), range(13, 15)]]
+
+        correct, correct_qi, correct_si = self.bruteforce_cdm(self.dist_matrix, query_ranges, series_ranges)
+
+        cdm = ContextualMatrixProfile(series_ranges, query_ranges)
+        self.mock_initialise(cdm)
+
+        for diag in range(-self.dist_matrix.shape[0] + 1, self.dist_matrix.shape[1]):
+            diag_ind = diag_indices_of(self.dist_matrix, diag)
+            cdm.process_diagonal(diag, self.dist_matrix[diag_ind])
+
+        npt.assert_allclose(cdm.distance_matrix, correct)
+        npt.assert_equal(cdm.match_index_query, correct_qi)
+        npt.assert_equal(cdm.match_index_series, correct_si)
+
+    def test_process_column(self):
+        query_ranges = [range(0, 4), range(7, 8)]
+        series_ranges = [range(1, 4), range(6, 8)]
+
+        correct, correct_qi, correct_si = self.bruteforce_cdm(self.dist_matrix, query_ranges, series_ranges)
+
+        cdm = ContextualMatrixProfile(series_ranges, query_ranges)
+        self.mock_initialise(cdm)
+
+        for column in range(0, self.dist_matrix.shape[1]):
+            cdm.process_column(column, self.dist_matrix[:, column])
+
+        npt.assert_allclose(cdm.distance_matrix, correct)
+        npt.assert_equal(cdm.match_index_query, correct_qi)
+        npt.assert_equal(cdm.match_index_series, correct_si)
+
+    def test_process_column_partial_calculation(self):
+        # Ranges selected so that the contexts will receive 0, 1, 2 or 3 columns
+        query_ranges = [range(0, 2), range(2, 5), range(7, 10)]
+        series_ranges = [range(0, 2), range(2, 4), range(4, 6), range(9, 14)]
+
+        part_dist_matrix = np.full_like(self.dist_matrix, np.inf)
+
+        cdm = ContextualMatrixProfile(series_ranges, query_ranges)
+        self.mock_initialise(cdm)
+
+        for column in [2, 3, 4, 5, 10, 11, 12]:
+            cdm.process_column(column, self.dist_matrix[:, column])
+            part_dist_matrix[:, column] = self.dist_matrix[:, column]
+
+        correct, correct_qi, correct_si = self.bruteforce_cdm(part_dist_matrix, query_ranges, series_ranges)
+
+        npt.assert_allclose(cdm.distance_matrix, correct)
+        npt.assert_equal(cdm.match_index_query, correct_qi)
+        npt.assert_equal(cdm.match_index_series, correct_si)
+
+    def test_process_column_complex_ranges(self):
+        # Overlapping ranges and contexts consisting of multiple ranges
+        query_ranges = [range(0, 10), range(1, 5), range(1, 2), range(4, 5),
+                        [range(1, 2), range(3, 4), range(7, 9)]]
+        series_ranges = [range(0, 2), range(1, 3), range(2, 4), range(3, 6), range(4, 8), range(4, 10),
+                         [range(0, 3), range(3, 5), range(13, 15)]]
+
+        correct, correct_qi, correct_si = self.bruteforce_cdm(self.dist_matrix, query_ranges, series_ranges)
+
+        cdm = ContextualMatrixProfile(series_ranges, query_ranges)
+        self.mock_initialise(cdm)
+
+        for column in range(0, self.dist_matrix.shape[1]):
+            cdm.process_column(column, self.dist_matrix[:, column])
+
+        npt.assert_allclose(cdm.distance_matrix, correct)
+        npt.assert_equal(cdm.match_index_query, correct_qi)
+        npt.assert_equal(cdm.match_index_series, correct_si)
