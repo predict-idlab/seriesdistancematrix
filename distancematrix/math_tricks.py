43465c0b8fd88fd4c92e2d57ae4df75ee142b9c5
Dieter De Paepe <dieter.depaepe@gmail.com>
2018-08-08 15:24:29 +0200

    Implement StreamingStats, for streaming mean.
    Numerical stability is hard to predict. Tests indicate
    that as long as very small (eg 1e-10) numbers aren't used,
    mean calculation is reliable.

--- distancematrix/math_tricks.py
+++ distancematrix/math_tricks.py
@@ -1,4 +1,5 @@
 import numpy as np
+from distancematrix.ringbuffer import RingBuffer
 
 _EPS = 1e-12
 
@@ -33,3 +34,42 @@ def sliding_mean_std(series, m):
     sliding_std = np.sqrt(sliding_std_sq)
 
     return sliding_avg, sliding_std
+
+
+class StreamingStats(object):
+    def __init__(self, series, m) -> None:
+        if m > series.shape[-1]:
+            raise RuntimeError("M should be <= series.shape[-1].")
+
+        self._data_buffer = RingBuffer(series)
+        self._m = m
+
+        sliding_avg, _ = sliding_mean_std(series, m)
+        self._mean_buffer = RingBuffer(sliding_avg)
+
+    def append(self, data):
+        buffer_length = self._data_buffer.view.shape[-1]
+        data_length = data.shape[-1]
+        if data_length >= buffer_length:
+            sliding_avg, _ = sliding_mean_std(data[..., -buffer_length:], self._m)
+            self._mean_buffer.push(sliding_avg)
+        else:
+            old_mean = self._mean_buffer.view[..., -1]
+            values_to_remove = self._data_buffer.view[..., -self._m: min(-1, -self._m + data_length)]
+            new_means = old_mean + np.cumsum(- values_to_remove + data[..., :values_to_remove.shape[-1]]) / self._m
+
+            self._mean_buffer.push(new_means)
+
+            if data_length >= self._m:
+                sliding_avg, _ = sliding_mean_std(data, self._m)
+                self._mean_buffer.push(sliding_avg)
+
+        self._data_buffer.push(data)
+
+    @property
+    def data(self):
+        return self._data_buffer.view
+
+    @property
+    def mean(self):
+        return self._mean_buffer.view
