900e244b5bbd734b68960e50d99c1cc334db732e
Dieter De Paepe <dieter.depaepe@gmail.com>
2018-09-07 16:29:40 +0200

    Add shortest path calculation for distance matrix.

--- distancematrix/tests/test_util.py
+++ distancematrix/tests/test_util.py
@@ -5,6 +5,9 @@ import numpy.testing as npt
 from distancematrix.util import diag_length
 from distancematrix.util import diag_indices
 from distancematrix.util import diag_indices_of
+from distancematrix.util import cut_indices_of
+from distancematrix.util import shortest_path_distances
+from distancematrix.util import shortest_path
 
 class TestUtil(TestCase):
     def test_diag_length_square_matrix(self):
@@ -69,3 +72,71 @@ class TestUtil(TestCase):
         npt.assert_equal(data[diag_indices_of(data, 1)], [2, 6])
         npt.assert_equal(data[diag_indices_of(data, 2)], [3])
         npt.assert_equal(data[diag_indices_of(data, 3)], [])
+
+    def test_cut_indices_of(self):
+        data = np.array([
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [10, 11, 12]
+        ])
+
+        npt.assert_equal(data[cut_indices_of(data, 0)], [1])
+        npt.assert_equal(data[cut_indices_of(data, 1)], [4, 2])
+        npt.assert_equal(data[cut_indices_of(data, 2)], [7, 5, 3])
+        npt.assert_equal(data[cut_indices_of(data, 3)], [10, 8, 6])
+        npt.assert_equal(data[cut_indices_of(data, 4)], [11, 9])
+        npt.assert_equal(data[cut_indices_of(data, 5)], [12])
+
+        npt.assert_equal(data[cut_indices_of(data, 6)], [])
+
+        data = np.array([
+            [0, 1, 2, 3, 4],
+            [5, 6, 7, 8, 9]
+        ])
+
+        npt.assert_equal(data[cut_indices_of(data, 0)], [0])
+        npt.assert_equal(data[cut_indices_of(data, 1)], [5, 1])
+        npt.assert_equal(data[cut_indices_of(data, 2)], [6, 2])
+        npt.assert_equal(data[cut_indices_of(data, 3)], [7, 3])
+        npt.assert_equal(data[cut_indices_of(data, 4)], [8, 4])
+        npt.assert_equal(data[cut_indices_of(data, 5)], [9])
+
+    def test_shortest_path_distances(self):
+        data = np.array([
+            [1, 2, 1, 0, 3],
+            [1, 3, 0, 1, 1],
+            [0, 1, 1, 4, 0],
+            [2, 5, 5, 2, 2],
+            [0, 1, 2, 3, 9]
+        ], dtype=np.float)
+
+        expected = np.array([
+            [1, 3, 4, 4, 7],
+            [2, 4, 3, 4, 5],
+            [2, 3, 4, 7, 4],
+            [4, 7, 8, 6, 6],
+            [4, 5, 7, 9, 15]
+        ], dtype=np.float)
+
+        result = shortest_path_distances(data)
+        npt.assert_equal(result, expected)
+
+        result = shortest_path_distances(data[:3, :])
+        npt.assert_equal(result, expected[:3, :])
+
+        result = shortest_path_distances(data[:, :3])
+        npt.assert_equal(result, expected[:, :3])
+
+    def test_shortest_path(self):
+        data = np.array([
+            [1, 2, 1, 0, 3],
+            [1, 3, 3, 1, 1],
+            [4, 3, 8, 4, 0],
+            [2, 2, 5, 2, 5],
+            [0, 1, 1, 3, 2],
+            [0, 1, 1, 5, 9]
+        ], dtype=np.float)
+
+        result = shortest_path(data)
+        npt.assert_equal(result, [[0, 0], [1, 0], [2, 1], [3, 1], [4, 2], [4, 3], [5, 4]])
--- distancematrix/util.py
+++ distancematrix/util.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def diag_length(h, w, diagonal=0):
     """
     Returns the number of elements on the specified diagonal of a matrix with dimensions (h, w).
@@ -42,3 +45,88 @@ def diag_indices_of(array, diagonal=0):
         raise RuntimeError("array should be 2D")
 
     return diag_indices(array.shape[0], array.shape[1], diagonal)
+
+
+def cut_indices_of(array, cut):
+    """
+    Calculates the indices of the elements on the given cut for the given matrix.
+    Where a diagonal runs from top left to bottom right, a cut runs from bottom left to top right.
+
+    :param array: 2D array
+    :param cut: index of the cut (cut 0 is the single element of the top left)
+    :return: the indices to retrieve the cut
+    """
+    if array.ndim != 2:
+        raise RuntimeError("array should be 2D")
+
+    h, w = array.shape
+
+    if cut < 0 or cut >= w + h - 1:
+        return range(0, 0), range(0, 0)
+
+    cut_length = cut + 1 - max(0, cut - h + 1) - max(0, cut - w + 1)
+
+    if cut < h:
+        return range(cut, cut - cut_length, -1), range(0, cut_length)
+    else:
+        return range(h-1, h-cut_length-1, -1), range(cut - h + 1, cut - h + 1 + cut_length)
+
+
+def shortest_path_distances(cost_array):
+    """
+    Creates a new array of the same shape, where each entry contains the lowest sum of elements on the path
+    from (0, 0) to that entry. Steps in the path can go horizontal, vertical and diagonal.
+
+    :param cost_array: 2D array containing only positives
+    :return: a new array
+    """
+    if cost_array.ndim != 2:
+        raise RuntimeError("array should be 2D")
+
+    dist = np.empty_like(cost_array, dtype=np.float)
+
+    # Borders can only come from previous step
+    dist[0, :] = np.cumsum(cost_array[0, :])
+    dist[:, 0] = np.cumsum(cost_array[:, 0])
+
+    # This operation could be vectorised by calculating one cut at a time, but the index juggling becomes quite
+    # complex for rectangular arrays.
+    for c in range(1, dist.shape[0]):
+        for r in range(1, dist.shape[1]):
+            dist[c, r] = min(dist[c-1, r], dist[c, r-1], dist[c-1, r-1]) + cost_array[c, r]
+
+    return dist
+
+
+def shortest_path(cost_array):
+    """
+    Finds the shortest (= least summed cost) path from the top left of the array to the bottom right.
+
+    :param cost_array: 2D array containing only positives
+    :return: array of indices, starting from the top left (index: [0, 0])
+    """
+    if cost_array.ndim != 2:
+        raise RuntimeError("array should be 2D")
+
+    row = cost_array.shape[0] - 1
+    col = cost_array.shape[1] - 1
+
+    walk_dist_matrix = shortest_path_distances(cost_array)
+
+    path = [(row, col)]
+    while row != 0 or col != 0:
+        best_cost = np.inf
+        if row != 0 and col != 0:
+            delta_step = (-1, -1)
+            best_cost = walk_dist_matrix[row - 1, col - 1]
+        if row != 0 and walk_dist_matrix[row - 1, col] < best_cost:
+            delta_step = (-1, 0)
+            best_cost = walk_dist_matrix[row - 1, col]
+        if col != 0 and walk_dist_matrix[row, col -1] < best_cost:
+            delta_step = (0, -1)
+
+        row += delta_step[0]
+        col += delta_step[1]
+        path.append((row, col))
+
+    return path[::-1] # TODO: other indices order
